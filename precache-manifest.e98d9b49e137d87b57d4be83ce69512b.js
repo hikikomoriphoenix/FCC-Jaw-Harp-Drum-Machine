390168174895d118df43fb47753b776a053119fe
Loremar Marabillas <loremar5858@gmail.com>
2020-04-20 15:36:20 +0800

    Updates

---
+++ precache-manifest.e98d9b49e137d87b57d4be83ce69512b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c399a80246981c04b3427142143e3133",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/index.html"
+  },
+  {
+    "revision": "c4e435834914cd57385c",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "855992c4bc639aab3631",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/css/main.4e70db36.chunk.css"
+  },
+  {
+    "revision": "c4e435834914cd57385c",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/js/2.e672c9ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/js/2.e672c9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855992c4bc639aab3631",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/js/main.71b14ab1.chunk.js"
+  },
+  {
+    "revision": "f3d0649dba10e795be5b",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/js/runtime-main.2ee906e7.js"
+  },
+  {
+    "revision": "6cab182a7e53d10c71eae899eb75e112",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-1.6cab182a.wav"
+  },
+  {
+    "revision": "f860e73f1c2ebf36ef0611a12d40e296",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-2.f860e73f.wav"
+  },
+  {
+    "revision": "a4de4166061428f5d6aa9d6e3173f9df",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-3.a4de4166.wav"
+  },
+  {
+    "revision": "82ab92272a9e832e30f2707dd5a4ff75",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-4.82ab9227.wav"
+  },
+  {
+    "revision": "ed5eab3a832eb90fcfb4ce7e601a7c4c",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-5.ed5eab3a.wav"
+  },
+  {
+    "revision": "21017986de7aa894cda0417473b53303",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-6.21017986.wav"
+  },
+  {
+    "revision": "10337beca919313380b48b769fb139e5",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-7.10337bec.wav"
+  },
+  {
+    "revision": "85e30d3ebb638f885010e5d86acfc378",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-8.85e30d3e.wav"
+  },
+  {
+    "revision": "773e124bb8f33325c93571a94fefabac",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-9.773e124b.wav"
+  },
+  {
+    "revision": "87580bb9fcf04146008af2932d92c602",
+    "url": "/FCC-Jaw-Harp-Drum-Machine/static/media/jaw-harp-drum-machine-border.87580bb9.svg"
+  }
+]);
